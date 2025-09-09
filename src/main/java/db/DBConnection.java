fc86a03f6362c3990f78286748c68fd203d6fd29
dilshansanjeewa <dilshansanjeewads7@gmail.com>
2025-09-09 22:29:08 +0530

    create singleton db connection class

---
+++ src/main/java/db/DBConnection.java
@@ -0,0 +1,26 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static DBConnection instance;
+    private final Connection connection;
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CeyluxResortdb", "root", "12345");
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if(instance == null){
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
